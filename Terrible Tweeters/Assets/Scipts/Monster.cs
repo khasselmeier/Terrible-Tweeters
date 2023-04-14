f140a19db80abe25f72201d647625abc786f7c0d
khasselmeier <123027444+khasselmeier@users.noreply.github.com>
2023-04-13 17:51:04 -0700

    Added audio, saving, pause menu

--- Terrible Tweeters/Assets/Scipts/Monster.cs
+++ Terrible Tweeters/Assets/Scipts/Monster.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
@@ -11,7 +12,25 @@ public class Monster : MonoBehaviour
 
     bool _hasDied;
 
-   void OnCollisionEnter2D(Collision2D collision)
+    void OnMouseDown()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    IEnumerator Start()
+    {
+        while(_hasDied == false)
+        {
+            float delay = UnityEngine.Random.Range(5, 30);
+            yield return new WaitForSeconds(delay);
+            if (_hasDied == false)
+            {
+                GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+       
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (ShouldDieFromCollision(collision))
         {
